3ef3699fb66a0f6db1f1ba9d1f87e3ed1e121960
Renish R <renobruez82@gmail.com>
2025-09-14 15:05:15 +0530

    Update Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -4,36 +4,24 @@ import "./Nav.css";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg custom-navbar">
-      <Link className="navbar-brand logo ms-3" to="/">
+    <nav className="custom-navbar">
+      <Link className="logo ms-3" to="/">
         🛒 Shopy
       </Link>
-      
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
 
-      <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
+      <div className="nav-links">
+        <ul>
+          <li>
             <Link className="nav-link" to="/productlist">
               Products
             </Link>
           </li>
-          <li className="nav-item">
+          <li>
             <Link className="nav-link" to="/aboutme">
               AboutMe
             </Link>
           </li>
-          <li className="nav-item">
+          <li>
             <Link className="nav-link logout-link" to="/login">
               Logout
             </Link>
